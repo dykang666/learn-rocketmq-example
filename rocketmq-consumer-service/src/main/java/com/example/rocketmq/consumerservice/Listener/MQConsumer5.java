5dd51cea15504d31b0211aa4c20e47720871fb69
dykang666 <kangdy@yuanian.com>
2025-01-12 18:02:53 +0800

    订单超时未支付，取消订单并释放库存 1、可以通过消息队列 2、也可以通过定时任务

--- rocketmq-consumer-service/src/main/java/com/example/rocketmq/consumerservice/Listener/MQConsumer5.java
+++ rocketmq-consumer-service/src/main/java/com/example/rocketmq/consumerservice/Listener/MQConsumer5.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Component;
  * @author kangdongyang
  * @version 1.0
  * @description:
+ * 订单超时未支付，取消订单并释放库存
+ *  1、可以通过消息队列
+ *  2、也可以通过定时任务
  * @date 2024/6/20 19:38
  */
 @Component
@@ -20,6 +23,31 @@ public class MQConsumer5 implements RocketMQListener<User> {
 
     @Override
     public void onMessage(User user) {
+        //
+        long  orderId=9999;
+        // 模拟检查订单是否超时未支付
+        boolean isOrderPaid = checkOrderPaymentStatus(orderId);  // 你可以根据业务逻辑实现该方法
+        if (!isOrderPaid) {
+            cancelOrder(orderId);  // 取消订单
+            releaseStock(orderId);  // 释放库存
+        }
+        //不做什么处理
         log.info("consumer-group-5 收到user类型消息:{}",user);
+    }
+
+    private static boolean checkOrderPaymentStatus(long orderId) {
+        // 这里可以根据订单ID查询数据库，判断该订单是否支付
+        return false;  // 假设订单未支付
+    }
+
+
+    private static void cancelOrder(long orderId) {
+        // 取消订单逻辑
+        System.out.println("取消订单: " + orderId);
+    }
+
+    private static void releaseStock(long orderId) {
+        // 释放库存逻辑
+        System.out.println("释放库存: " + orderId);
     }
 }
